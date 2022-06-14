75cccc2373aed0378474960ae21d4b8219a038f1
carlyemile <carlyemile@gmail.com>
2022-06-14 12:02:09 -0400

    Changing live site to dynamically create links for all projects found

---
+++ index.js
@@ -0,0 +1,24 @@
+const linksContainer = document.getElementById("links");
+const folder = "projects/";
+
+$.ajax({
+    url : folder,
+    success: function (data) {
+        $(data).find("a").attr("href", function (i, val) {
+            if(val.includes("/projects/")){
+                console.log(val);
+                renderProjectLink(val);
+            }
+        });
+    }
+});
+
+function renderProjectLink(projectPath) {
+    let projectName = projectPath.replace("/projects/", "").replaceAll("-", " ");
+    let projectUrl = projectPath.replace("/", "") + "/index.html";
+    console.log(projectName);
+    linksContainer.innerHTML = `<a href="${projectUrl}" class="project-link">${projectName}</a>`;
+    console.log(projectUrl)
+    console.log(linksContainer.innerHtml)
+}
+
